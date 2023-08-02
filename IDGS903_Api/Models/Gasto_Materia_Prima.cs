7aab44976bdb7d618d3d7e50f582444877d45105
IDGS-803-20001480 <jonathanutlpruebas@gmail.com>
2023-08-01 19:06:11 -0600

    cambios de los modelos en sus atribultos de float a double para evitar errores

--- IDGS903_Api/Models/Gasto_Materia_Prima.cs
+++ IDGS903_Api/Models/Gasto_Materia_Prima.cs
@@ -6,8 +6,8 @@ namespace IDGS903_Api.Models
     {
         [Key]
         public int Id { get; set; }
-        public float Cantidad { get; set; }
-        public float Costo { get; set; }
+        public Double Cantidad { get; set; }
+        public Double Costo { get; set; }
         public int MateriaPrima_id { get; set; }
         public int Producto_id { get; set; }
     }
--- IDGS903_Api/Models/materiaPrima.cs
+++ IDGS903_Api/Models/materiaPrima.cs
@@ -7,8 +7,8 @@ namespace IDGS903_Api.Models
         [Key]
         public int Id { get; set; }
         public string? Nombre { get; set; }
-        public float Costo { get; set; }
-        public float Stock { get; set; }
+        public Double Costo { get; set; }
+        public Double Stock { get; set; }
         public int TipoMateriaPrima_id { get; set; }
         public int Proveedor_id { get; set; }
 
